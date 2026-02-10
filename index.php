f457c246df69f73c2f5939a37b18833750aa605b
Kevin <chegekevv@gmail.com>
2026-02-10 23:08:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -18,13 +18,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['username'])) {
         <h1 id="welcome">SYSTEM ACCESS</h1>
         <form action="index.php" method="POST" class="login-form">
             <label>CODENAME:</label>
-            <input type="text" name="username" required autocomplete="off">
+            <input type="text" name="username" required autocomplete="off" placeholder="DONT SHARE">
             
             <label>ENCRYPTION KEY:</label>
-            <input type="password" required>
+            <input type="password" required placeholder="..KEY..">
             
             <input type="submit" value="PROCEED" class="send-btn">
         </form>
     </div>
 </body>
 </html>
+
